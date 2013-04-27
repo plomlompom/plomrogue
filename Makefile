94be953e83260d2a5dbce9e6a9443ebe0c41cd2d
Christian Heller <c.heller@plomlompom.de>
2013-04-27 05:23:58 +0200

    Humble beginnings of customizable keybindings. So far only reading from, not writing to file. Window only shows key number codes, not key names.

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 roguelike:
 	cc -g -o windows.o -c windows.c
 	cc -g -o roguelike.o -c roguelike.c
-	cc -g -o roguelike windows.o roguelike.o -lncurses
+	cc -g -o roguelike windows.o roguelike.o -lncurses -lm
 
 clean:
 	rm *.o; rm roguelike
--- roguelike.c
+++ roguelike.c
@@ -2,6 +2,7 @@
 #include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 #include "windows.h"
 
 struct Map {
@@ -12,6 +13,19 @@ struct Map {
   int player_x;
   int player_y;
   char * cells; };
+
+struct KeyBinding {
+  char * name;
+  int key; };
+
+struct KeysWinData {
+  int max;
+  char edit;
+  int select; };
+
+struct World {
+  struct KeyBinding * keybindings;
+  struct KeysWinData * keyswindata; };
 
 void draw_with_linebreaks (struct Win * win, char * text, int start_y) {
 // Write text into window content space. Start on row start_y. Fill unused rows with whitespace.
@@ -151,11 +165,117 @@ void update_log (struct Win * win, char * text) {
   free(win->data);
   win->data = new_text; }
 
+int get_action_key (struct KeyBinding * keybindings, char * name) {
+// Return key matching name in keybindings.
+  int i = 0;
+  while (strcmp(keybindings[i].name, name) )
+    i++;
+  return keybindings[i].key; }
+
+void draw_keys_window (struct Win * win) {
+// Draw keybinding window.
+  struct World * world = (struct World *) win->data;
+  struct KeysWinData * keyswindata = (struct KeysWinData *) world->keyswindata;
+  struct KeyBinding * keybindings = world->keybindings;
+  int height_av = win->height - 1;
+  int width_av = win->width - 1;
+  int offset = 0;
+  if (keyswindata->max >= height_av) {
+    if (keyswindata->select > height_av / 2)
+      if (keyswindata->select < (keyswindata->max - (height_av / 2)))
+        offset = keyswindata->select - (height_av / 2);
+      else
+        offset = keyswindata->max - height_av + 1; }
+  int keydescwidth = 3;
+  char * keydesc = malloc(keydescwidth + 1);
+  attr_t attri;
+  int y, x;
+  for (y = 0; 0 != keybindings[offset + y].name && y < height_av; y++) {
+    attri = 0;
+    if (y == keyswindata->select - offset) {
+      attri = A_REVERSE;
+      if (1 == keyswindata->edit)
+        attri = attri | A_BLINK; }
+    snprintf(keydesc, keydescwidth + 1, "%3d         ", keybindings[y + offset].key);
+    for (x = 0; x < width_av; x++)
+      if (strlen(keydesc) > x)
+        mvwaddch(win->curses_win, y + 1, x + win->border_left, keydesc[x] | attri);
+      else if (strlen(keydesc) < x && x < strlen(keybindings[y + offset].name) + strlen(keydesc) + 1)
+        mvwaddch(win->curses_win, y + 1, x + win->border_left, keybindings[y + offset].name[x - strlen(keydesc) - 1] | attri);
+      else
+        mvwaddch(win->curses_win, y + 1, x + win->border_left, ' ' | attri); }
+  free(keydesc);
+  if (y < height_av)
+    for (; y < height_av; y++)
+      for (x = 0; x < width_av; x++)
+        mvwaddch(win->curses_win, y + 1, x + win->border_left, ' '); }
+
+void init_keybindings(struct World * world) {
+// Initialize keybindings from file "keybindings".
+  FILE * file = fopen("keybindings", "r");
+  int lines = 0;
+  int c = 0;
+  while (EOF != c) {
+    c = getc(file);
+    if ('\n' == c)
+      lines++; }
+  struct KeyBinding * keybindings = malloc(lines * sizeof(struct KeyBinding));
+  fseek(file, 0, SEEK_SET);
+  char * command = malloc(100);
+  char commcount = 0;
+  char * digits = malloc(10);
+  char digicount = 0;
+  int key, digimax;
+  int keycount = 0;
+  c = getc(file);
+  while (EOF != c) {
+    if ('\n' == c) {
+      keybindings[keycount].name = calloc(commcount, sizeof(char));
+      memcpy(keybindings[keycount].name, command, commcount);
+      keybindings[keycount].key = key;
+      keycount++;
+      digicount = 0;
+      commcount = 0; }
+    else if (-1 != digicount)
+      if (' ' == c) {
+        key = 0;
+        digimax = digicount - 1;
+        while (digicount > 0) {
+          digicount--;
+          key = key + ((digits[digicount] - 48) * pow(10, digimax - digicount)); }
+        digicount = -1; }
+      else {
+        digits[digicount] = c;
+        digicount++; }
+    else {
+      command[commcount] = c;
+      commcount++; }
+    c = getc(file); }
+  free(command);
+  free(digits);
+  fclose(file);
+  struct KeysWinData * keyswindata = malloc(sizeof(struct KeysWinData));
+  keyswindata->max = lines - 1;
+  keyswindata->select = 0;
+  keyswindata->edit = 0;
+  world->keybindings = keybindings;
+  world->keyswindata = keyswindata; }
+
 int main () {
+  struct World world;
+  init_keybindings(&world);
+
   WINDOW * screen = initscr();
   noecho();
   curs_set(0);
+  keypad(screen, TRUE);
+  raw();
+
   struct WinMeta win_meta = init_win_meta(screen);
+
+  struct Win win_keys = init_window(&win_meta, "Keys");
+  win_keys.draw = draw_keys_window;
+  win_keys.data = &world;
 
   struct Win win_map = init_window(&win_meta, "Map");
   win_map.draw = draw_map;
@@ -172,69 +292,84 @@ int main () {
   win_log.data = calloc(1, sizeof(char));
   update_log (&win_log, "Start!");
 
-  char key;
+  int key;
   while (1) {
     key = getch();
-    if      (key == 'Q')                                    // quit
+    if      (key == get_action_key(world.keybindings, "quit"))
       break;
-    else if (key == '1')                                    // toggle map window
+    else if (key == get_action_key(world.keybindings, "toggle keys window"))
+      toggle_window(&win_meta, &win_keys);
+    else if (key == get_action_key(world.keybindings, "toggle map window"))
       toggle_window(&win_meta, &win_map);
-    else if (key == '2')                                    // toggle info window
+    else if (key == get_action_key(world.keybindings, "toggle info window"))
       toggle_window(&win_meta, &win_info);
-    else if (key == '3')                                    // toggle log window
+    else if (key == get_action_key(world.keybindings, "toggle log window"))
       toggle_window(&win_meta, &win_log);
-    else if (key == 'x') {                                  // scroll map down
-      map.offset_y++;
+    else if (key == get_action_key(world.keybindings, "cycle forwards") && win_meta.active != 0)
+      cycle_active_window(&win_meta, 'n');
+    else if (key == get_action_key(world.keybindings, "cycle backwards") && win_meta.active != 0)
+      cycle_active_window(&win_meta, 'p');
+    else if (key == get_action_key(world.keybindings, "shift forwards")  && win_meta.active != 0)
+      shift_window(&win_meta, 'f');
+    else if (key == get_action_key(world.keybindings, "shift backwards") && win_meta.active != 0)
+      shift_window(&win_meta, 'b');
+    else if (key == get_action_key(world.keybindings, "grow hor") && win_meta.active != 0)
+      resize_window(&win_meta, '*');
+    else if (key == get_action_key(world.keybindings, "shrink hor") && win_meta.active != 0)
+      resize_window(&win_meta, '_');
+    else if (key == get_action_key(world.keybindings, "grow ver") && win_meta.active != 0)
+      resize_window(&win_meta, '+');
+    else if (key == get_action_key(world.keybindings, "shrink ver") && win_meta.active != 0)
+      resize_window(&win_meta, '-');
+    else if (key == get_action_key(world.keybindings, "keys nav up") && world.keyswindata->select > 0) {
+      world.keyswindata->select--;
       draw_all_windows (&win_meta); }
-    else if (key == 'w' && map.offset_y > 0) {              // scroll map up
+    else if (key == get_action_key(world.keybindings, "keys nav down") && world.keyswindata->select < world.keyswindata->max) {
+      world.keyswindata->select++;
+      draw_all_windows (&win_meta); }
+    else if (key == get_action_key(world.keybindings, "keys mod")) {
+      world.keyswindata->edit = 1;
+      draw_all_windows (&win_meta);
+      key = getch();
+      world.keybindings[world.keyswindata->select].key = key;
+      world.keyswindata->edit = 0;
+      draw_all_windows (&win_meta); }
+    else if (key == get_action_key(world.keybindings, "map up") && map.offset_y > 0) {
       map.offset_y--;
       draw_all_windows (&win_meta); }
-    else if (key == 'd') {                                  // scroll map right
+    else if (key == get_action_key(world.keybindings, "map down")) {
+      map.offset_y++;
+      draw_all_windows (&win_meta); }
+    else if (key == get_action_key(world.keybindings, "map right")) {
       map.offset_x++;
       draw_all_windows (&win_meta); }
-    else if (key == 'a' && map.offset_x > 0) {              // scroll map left
+    else if (key == get_action_key(world.keybindings, "map left") && map.offset_x > 0) {
       map.offset_x--;
       draw_all_windows (&win_meta); }
-    else if (key == 'b' && map.player_y < map.height - 1) { // move player south
+    else if (key == get_action_key(world.keybindings, "player down") && map.player_y < map.height - 1) {
       update_info (&win_info);
       update_log (&win_log, "\nYou move south.");
       map.player_y++;
       draw_all_windows (&win_meta); }
-    else if (key == 't' && map.player_y > 0) {              // move player north
+    else if (key == get_action_key(world.keybindings, "player up") && map.player_y > 0) {
       update_info (&win_info);
       update_log (&win_log, "\nYou move north.");
       map.player_y--;
       draw_all_windows (&win_meta); }
-    else if (key == 'h' && map.player_x < map.width - 1) {  // move player east
+    else if (key == get_action_key(world.keybindings, "player right") && map.player_x < map.width - 1) {
       update_info (&win_info);
       update_log (&win_log, "\nYou move east.");
       map.player_x++;
       draw_all_windows (&win_meta); }
-    else if (key == 'f' && map.player_x > 0) {              // move player west
+    else if (key == get_action_key(world.keybindings, "player left") && map.player_x > 0) {
       update_info (&win_info);
       update_log (&win_log, "\nYou move west.");
       map.player_x--;
       draw_all_windows (&win_meta); }
-    else if (key == '.') {                                  // wait
+    else if (key == get_action_key(world.keybindings, "wait") ) {
       update_info (&win_info);
       update_log (&win_log, "\nYou wait.");
-      draw_all_windows(&win_meta); }
-    else if (key == '>' && win_meta.active != 0)            // cycle forwards in windows chain
-      cycle_active_window(&win_meta, 'n');
-    else if (key == '<' && win_meta.active != 0)            // cycle backwards in windows chain
-      cycle_active_window(&win_meta, 'p');
-    else if (key == 'y' && win_meta.active != 0)            // shift window forwards
-      shift_window(&win_meta, 'f');
-    else if (key == 'Y' && win_meta.active != 0)            // shift window backwards
-      shift_window(&win_meta, 'b');
-    else if (key == '*' && win_meta.active != 0)            // grow window horizontally
-      resize_window(&win_meta, '*');
-    else if (key == '_' && win_meta.active != 0)            // shrink window horizontally
-      resize_window(&win_meta, '_');
-    else if (key == '+' && win_meta.active != 0)            // grow window vertically
-      resize_window(&win_meta, '+');
-    else if (key == '-' && win_meta.active != 0)            // shrink window vertically
-      resize_window(&win_meta, '-'); }
+      draw_all_windows(&win_meta); } }
 
   endwin();
   return 0; }
