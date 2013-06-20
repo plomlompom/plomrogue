f743c2cc4451192d645e726654a48bd58a636cef
Christian Heller <c.heller@plomlompom.de>
2013-06-20 03:39:37 +0200

    Use preprocessor macro instead of constant variable for uchar size.

--- src/readwrite.c
+++ src/readwrite.c
@@ -1,40 +1,36 @@
 #include <stdio.h>
 #include <limits.h>
 #include <stdint.h>
+#define UCHARSIZE (UCHAR_MAX + 1)
 
 uint16_t read_uint16_bigendian(FILE * file) {
 // Read uint16 from file in big-endian order.
-  const uint16_t nchar = UCHAR_MAX + 1;
   unsigned char a = fgetc(file);
   unsigned char b = fgetc(file);
-  return (a * nchar) + b; }
+  return (a * UCHARSIZE) + b; }
 
 void write_uint16_bigendian(uint16_t x, FILE * file) {
 // Write uint16 to file in beg-endian order.
-  const uint16_t nchar = UCHAR_MAX + 1;
-  unsigned char a = x / nchar;
-  unsigned char b = x % nchar;
+  unsigned char a = x / UCHARSIZE;
+  unsigned char b = x % UCHARSIZE;
   fputc(a, file);
   fputc(b, file); }
 
 uint32_t read_uint32_bigendian(FILE * file) {
 // Read uint32 from file in big-endian order.
-  const uint16_t nchar = UCHAR_MAX + 1;
   unsigned char a = fgetc(file);
   unsigned char b = fgetc(file);
   unsigned char c = fgetc(file);
   unsigned char d = fgetc(file);
-  return (a * nchar * nchar * nchar) + (b * nchar * nchar) + (c * nchar) + d; }
+  return (a * UCHARSIZE * UCHARSIZE * UCHARSIZE) + (b * UCHARSIZE * UCHARSIZE) + (c * UCHARSIZE) + d; }
 
 void write_uint32_bigendian(uint32_t x, FILE * file) {
 // Write uint32 to file in beg-endian order.
-  const uint16_t nchar = UCHAR_MAX + 1;
-  unsigned char a = x / (nchar * nchar * nchar);
-  unsigned char b = (x - (a * nchar * nchar * nchar)) / (nchar * nchar);
-  unsigned char c = (x - ((a * nchar * nchar * nchar) + (b * nchar * nchar))) / nchar;
-  unsigned char d = x % nchar;
+  unsigned char a = x / (UCHARSIZE * UCHARSIZE * UCHARSIZE);
+  unsigned char b = (x - (a * UCHARSIZE * UCHARSIZE * UCHARSIZE)) / (UCHARSIZE * UCHARSIZE);
+  unsigned char c = (x - ((a * UCHARSIZE * UCHARSIZE * UCHARSIZE) + (b * UCHARSIZE * UCHARSIZE))) / UCHARSIZE;
+  unsigned char d = x % UCHARSIZE;
   fputc(a, file);
   fputc(b, file);
   fputc(c, file);
   fputc(d, file); }
-
