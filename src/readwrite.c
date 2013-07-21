ffc1419854e5166eef5ea07ed0fb5be53a1826f3
Christian Heller <c.heller@plomlompom.de>
2013-07-21 03:42:49 +0200

    Forced new documentation style on readwrite library.

--- src/readwrite.c
+++ src/readwrite.c
@@ -1,3 +1,5 @@
+/*  readwrite.c */
+
 #include "readwrite.h"
 #include <stdio.h>
 #include <stdint.h>
@@ -6,13 +8,11 @@
 static const uint16_t uchar_s = UCHAR_MAX + 1;
 
 extern uint16_t read_uint16_bigendian(FILE * file) {
-// Read uint16 from file in big-endian order.
   unsigned char a = fgetc(file);
   unsigned char b = fgetc(file);
   return (a * uchar_s) + b; }
 
 extern uint32_t read_uint32_bigendian(FILE * file) {
-// Read uint32 from file in big-endian order.
   unsigned char a = fgetc(file);
   unsigned char b = fgetc(file);
   unsigned char c = fgetc(file);
@@ -20,14 +20,12 @@ extern uint32_t read_uint32_bigendian(FILE * file) {
   return (a * uchar_s * uchar_s * uchar_s) + (b * uchar_s * uchar_s) + (c * uchar_s) + d; }
 
 extern void write_uint16_bigendian(uint16_t x, FILE * file) {
-// Write uint16 to file in beg-endian order.
   unsigned char a = x / uchar_s;
   unsigned char b = x % uchar_s;
   fputc(a, file);
   fputc(b, file); }
 
 extern void write_uint32_bigendian(uint32_t x, FILE * file) {
-// Write uint32 to file in beg-endian order.
   unsigned char a = x / (uchar_s * uchar_s * uchar_s);
   unsigned char b = (x - (a * uchar_s * uchar_s * uchar_s)) / (uchar_s * uchar_s);
   unsigned char c = (x - ((a * uchar_s * uchar_s * uchar_s) + (b * uchar_s * uchar_s))) / uchar_s;
--- src/readwrite.h
+++ src/readwrite.h
@@ -1,3 +1,9 @@
+/*   readwrite.h:
+ *
+ *   Routines for reading/writing multibyte data from/to files. They ensure a
+ *   defined endianness.
+ */
+
 #ifndef READWRITE_H
 #define READWRITE_H
 
@@ -6,7 +12,7 @@
 
 extern uint16_t read_uint16_bigendian(FILE * file);
 extern uint32_t read_uint32_bigendian(FILE * file);
-extern void write_uint16_bigendian(uint16_t x, FILE * file);
-extern void write_uint32_bigendian(uint32_t x, FILE * file);
+extern void     write_uint16_bigendian(uint16_t x, FILE * file);
+extern void     write_uint32_bigendian(uint32_t x, FILE * file);
 
 #endif
