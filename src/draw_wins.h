289cfa930b552dbe0a1755a1153a792280db3b54
Christian Heller <c.heller@plomlompom.de>
2013-07-29 03:10:07 +0200

    Minor formatting change.

--- src/draw_wins.h
+++ src/draw_wins.h
@@ -31,17 +31,17 @@ extern void draw_log_win(struct Win * win);
 
 
 /* Draw game map and actors/objects on it into "win". Respect scroll offset. */
-extern void draw_map_win (struct Win * win);
+extern void draw_map_win(struct Win * win);
 
 
 
 /* Draw into "win" the game / player status infos. */
-extern void draw_info_win (struct Win * win);
+extern void draw_info_win(struct Win * win);
 
 
 
 /* Draw keybindings selection/manipulation menu. */
-extern void draw_keys_win (struct Win * win);
+extern void draw_keys_win(struct Win * win);
 
 
 
