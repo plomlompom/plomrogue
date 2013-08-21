a592eadef06988e650ae09de0f9f5abffb482663
Christian Heller <c.heller@plomlompom.de>
2013-08-21 21:10:05 +0200

    Re-styled code.

--- src/yx_uint16.c
+++ src/yx_uint16.c
@@ -9,9 +9,13 @@
 extern char yx_uint16_cmp(struct yx_uint16 a, struct yx_uint16 b)
 {
     if (a.y == b.y && a.x == b.x)
+    {
         return 1;
+    }
     else
+    {
         return 0;
+    }
 }
 
 
@@ -19,12 +23,20 @@ extern char yx_uint16_cmp(struct yx_uint16 a, struct yx_uint16 b)
 extern struct yx_uint16 mv_yx_in_dir(enum dir d, struct yx_uint16 yx)
 {
     if      (d == NORTH)
+    {
         yx.y--;
+    }
     else if (d == EAST)
+    {
         yx.x++;
+    }
     else if (d == SOUTH)
+    {
         yx.y++;
+    }
     else if (d == WEST)
+    {
         yx.x--;
+    }
     return yx;
 }
