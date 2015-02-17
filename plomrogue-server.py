9b494116de1a99e3abb5c91136d4c624483593d0
Christian Heller <c.heller@plomlompom.de>
2015-02-17 01:43:43 +0100

    Server/py: Implement C's detect_atomic_leftover(), part of run_game().

--- plomrogue-server.py
+++ plomrogue-server.py
@@ -3,6 +3,12 @@ import os
 import time
 
 
+class HandledException(Exception):
+    """Feature-less Exception child. Use for expected operational errors."""
+
+    pass
+
+
 def setup_server_io(io_db):
     """Fill IO files DB with proper file( path)s. Write process IO test string.
 
@@ -38,12 +44,35 @@ def cleanup_server_io(io_db):
     if os.access(io_db["path_worldstate"], os.F_OK):  # world is active, which
         os.remove(io_db["path_worldstate"])           # is not guaranteed.
 
+
+def detect_atomic_leftover(path):
+    """Raise HandledException if file is found at path + "_tmp"."""
+    path_tmp = path + "_tmp"
+    msg = "Found file '" + path_tmp + "' that may be a leftover from an " \
+          "aborted previous attempt to write '" + path + "'. Aborting until " \
+          "the matter is resolved by removing it from its current path."
+    if os.access(path_tmp, os.F_OK):
+        raise HandledException(msg)
+
+
 io_db = {}
 try:
     print("DUMMY: Obey command-line arguments.")
     print("DUMMY: Open files.")
     setup_server_io(io_db)
     print("DUMMY: Run game.")
+    path_recordfile = "recordfile"
+    path_savefile = "savefile"
+    detect_atomic_leftover(path_savefile)
+    detect_atomic_leftover(path_recordfile)
+    if os.access(path_savefile, os.F_OK):
+        print(open(path_savefile, "r").read())
+    else:
+        msg = "MAKE_WORLD " + str(int(time.time()))
+        print(msg)
+except HandledException as exception:
+    print("Error:")
+    print(exception.args[0])
 except:
     print("SOMETHING WENT WRONG\n")
     raise
