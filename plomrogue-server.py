6da06167fdafc4b2f197657bb2af431806b0d5fc
Christian Heller <c.heller@plomlompom.de>
2015-02-22 12:21:59 +0100

    Server/py: In replay mode, replay until world turn >= replay argument.

--- plomrogue-server.py
+++ plomrogue-server.py
@@ -95,17 +95,30 @@ def record(cmd, io_db):
     os.rename(path_tmp, io_db["path_record"])
 
 
-def obey_lines_in_file(path, name):
-    """Call obey() on each line of path's file, use name in input prefix."""
-    file = open(io_db["path_worldconf"], "r")
+def obey_lines_in_file(path, name, break_test = None):
+    """Call obey() on each line of path's file, use name in input prefix.
+
+    If break_test function is set, only read the file until it returns True.
+    """
+    file = open(path, "r")
     line_n = 1
     for line in file.readlines():
+        if None != break_test and break_test():
+            break
         obey(line.rstrip(), io_db, name + "file line " + str(line_n))
         line_n = line_n + 1
     file.close()
 
 
+def make_turn_tester(turn_to_compare, world_db):
+    """Return tester whether world_db["turn"] greater/equal turn_to_compare."""
+    def turn_tester():
+        return world_db["turn"] >= turn_to_compare
+    return turn_tester
+
+
 io_db = {}
+world_db = {}
 try:
     parser = argparse.ArgumentParser()
     parser.add_argument('-s', nargs='?', type=int, dest='replay', const=1,
@@ -122,6 +135,9 @@ try:
               " (if so late a turn is to be found).")
         if not os.access(io_db["path_record"], os.F_OK):
             raise SystemExit("No record file found to replay.")
+        world_db["turn"] = 0
+        break_tester = make_turn_tester(opts.replay, world_db)
+        obey_lines_in_file(io_db["path_record"], "record ", break_tester)
     else:
         if os.access(io_db["path_save"], os.F_OK):
             obey_lines_in_file(io_db["path_save"], "save")
