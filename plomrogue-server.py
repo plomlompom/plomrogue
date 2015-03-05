555930c81055c3eab2fb3daa55cf3343faec5fd2
Christian Heller <c.heller@plomlompom.de>
2015-03-05 13:58:38 +0100

    Server/py: Undummify remake_map().

--- plomrogue-server.py
+++ plomrogue-server.py
@@ -395,8 +395,62 @@ def play_game():
 
 
 def remake_map():
-    # DUMMY map creator.
-    world_db["MAP"] = bytearray(b'.' * (world_db["MAP_LENGTH"] ** 2))
+    """(Re-)make island map.
+
+    Let "~" represent water, "." land, "X" trees: Build island shape randomly,
+    start with one land cell in the middle, then go into cycle of repeatedly
+    selecting a random sea cell and transforming it into land if it is neighbor
+    to land. The cycle ends when a land cell is due to be created at the map's
+    border. Then put some trees on the map (TODO: more precise algorithm desc).
+    """
+    def is_neighbor(coordinates, type):
+        y = coordinates[0]
+        x = coordinates[1]
+        length = world_db["MAP_LENGTH"]
+        ind = y % 2
+        diag_west = x + (ind > 0)
+        diag_east = x + (ind < (length - 1))
+        pos = (y * length) + x
+        if (y > 0 and diag_east
+            and type == chr(world_db["MAP"][pos - length + ind])) \
+           or (x < (length - 1)
+               and type == chr(world_db["MAP"][pos + 1])) \
+           or (y < (length - 1) and diag_east
+               and type == chr(world_db["MAP"][pos + length + ind])) \
+           or (y > 0 and diag_west
+               and type == chr(world_db["MAP"][pos - length - (not ind)])) \
+           or (x > 0
+               and type == chr(world_db["MAP"][pos - 1])) \
+           or (y < (length - 1) and diag_west
+               and type == chr(world_db["MAP"][pos + length - (not ind)])):
+            return True
+        return False
+    store_seed = rand.seed
+    world_db["MAP"] = bytearray(b'~' * (world_db["MAP_LENGTH"] ** 2))
+    length = world_db["MAP_LENGTH"]
+    add_half_width = (not (length % 2)) * int(length / 2)
+    world_db["MAP"][int((length ** 2) / 2) + add_half_width] = ord(".")
+    while (1):
+        y = rand.next() % length
+        x = rand.next() % length
+        pos = (y * length) + x
+        if "~" == chr(world_db["MAP"][pos]) and is_neighbor((y, x), "."):
+            if y == 0 or y == (length - 1) or x == 0 or x == (length - 1):
+                break
+            world_db["MAP"][pos] = ord(".")
+    n_trees = int((length ** 2) / 16)
+    i_trees = 0
+    while (i_trees <= n_trees):
+        single_allowed = rand.next() % 32
+        y = rand.next() % length
+        x = rand.next() % length
+        pos = (y * length) + x
+        if "." == chr(world_db["MAP"][pos]) \
+          and ((not single_allowed) or is_neighbor((y, x), "X")):
+            world_db["MAP"][pos] = ord("X")
+            i_trees += 1
+    rand.seed = store_seed
+    # This all-too-precise replica of the original C code misses iter_limit().
 
 
 def update_map_memory(t):
