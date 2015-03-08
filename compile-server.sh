34cfc4806ae26ab334464cb0a020e443f03e4173
Christian Heller <c.heller@plomlompom.de>
2015-03-08 19:39:44 +0100

    Server/py: Use -O3 for libplomrogue.

--- compile-server.sh
+++ compile-server.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-gcc -shared -fPIC -std=c11 -pedantic-errors -Wall -Werror -Wextra -Wformat-security -g -o libplomrogue.so libplomrogue.c
+gcc -shared -fPIC -std=c11 -pedantic-errors -Wall -Werror -Wextra -Wformat-security -O3 -o libplomrogue.so libplomrogue.c
