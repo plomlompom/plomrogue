a45e6ee435f666be3ccf13264d4e0c9ab9e1e6e0
Christian Heller <c.heller@plomlompom.de>
2015-12-16 02:02:58 +0100

    New client: Add quick help text to log start.

--- client_prototype.py
+++ client_prototype.py
@@ -565,7 +565,21 @@ world_data = {
     "look": [],
     "look_mode": False,
     "look_scroll": 0,
-    "log": [],
+    "log": [
+"QUICK COMMAND OVERVIEW: "
+"Move through map with 'w', 'e', 's', 'd', 'x', 'c'. "
+"Pick up things with 'P', drop a thing selected from the inventory with 'D' "
+"or use it with 'P'. "
+"Move through inventory selection with 'j' and 'k'. "
+"Toggle looking around mode with 'l'. "
+"Scroll 'Things here' window with 'J' and 'K'. "
+"Let AI decide next move with 'A'. "
+"Quit with 'Q'. ",
+"STATS OVERVIEW: "
+"'T': Turn; 'H': Health (the higher, the better); "
+"'S': Satiation (the closer to zero, the better).",
+"See README file for more help."
+],
     "map_center": [-1, -1],
     "map_size": 0,
     "mem_map": "",
