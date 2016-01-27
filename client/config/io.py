cdf875d1b67cfced25569caccc74f4c45417dc96
Christian Heller <c.heller@plomlompom.de>
2016-01-27 23:10:10 +0100

    Client: Make worldstate file data reading order configurable.

--- client/config/io.py
+++ client/config/io.py
@@ -1,5 +1,15 @@
 io = {
     "path_out": "server/in",
     "path_in": "server/out",
-    "path_worldstate": "server/worldstate"
+    "path_worldstate": "server/worldstate",
+    "worldstate_read_order": [
+        ["lifepoints", "int"],
+        ["satiation", "int"],
+        ["inventory", "lines"],
+        ["avatar_position", "int", 0],
+        ["avatar_position", "int", 1],
+        ["map_size", "int"],
+        ["fov_map", "map"],
+        ["mem_map", "map"]
+    ]
 }
