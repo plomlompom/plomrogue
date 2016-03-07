31f6bf5bcdc93f9e5871a1d6e6faca39efc04f46
Christian Heller <c.heller@plomlompom.de>
2016-03-07 01:51:16 +0100

    PtIG, TCE: Add license notices.

--- plugins/client/PleaseTheIslandGod.py
+++ plugins/client/PleaseTheIslandGod.py
@@ -1,3 +1,8 @@
+# This file is part of PlomRogue. PlomRogue is licensed under the GPL version 3
+# or any later version. For details on its copyright, license, and warranties,
+# see the file NOTICE in the root directory of the PlomRogue source package.
+
+
 from client.config.io import io
 from client.config.world_data import world_data
 world_data["log"][-2] = "STATS OVERVIEW: " \
--- plugins/client/TheCrawlingEater.py
+++ plugins/client/TheCrawlingEater.py
@@ -1,3 +1,8 @@
+# This file is part of PlomRogue. PlomRogue is licensed under the GPL version 3
+# or any later version. For details on its copyright, license, and warranties,
+# see the file NOTICE in the root directory of the PlomRogue source package.
+
+
 def win_map(self):
     win_size = self.size
     offset = [0, 0]
